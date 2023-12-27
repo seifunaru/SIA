a739b956038211b7ae5311bdd6a15d6252d6e659
seifunaru <seifu.business@gmail.com>
2023-12-27 11:40:19 +0100

    StepMaager first upload, serves as connections backend.

---
+++ data/stepmanager.cpp
@@ -0,0 +1,67 @@
+#include "stepmanager.h"
+
+#include <QObject>
+#include <QVector>
+#include <QString>
+#include <QDebug>
+
+// This vector of strings tracks which options have been chosen on all steps.
+QVector <QString> selectedOptions;
+
+StepManager::StepManager(QObject *parent) : QObject(parent)
+{
+
+}
+
+
+
+
+//This function sets a chosen step by the user on selectedOptions vector.
+void StepManager::setStep(QString currentStep)
+{
+    if (!selectedOptions.contains(currentStep)) {
+        selectedOptions.append(currentStep);
+    } else {
+        qDebug() << "Element [" + currentStep + "] is already selected.";
+    }
+}
+
+
+
+
+void StepManager::doNextStep()
+{
+    if (stepCount != 0) {
+
+    } else {
+        stepCount++;
+        emit stackNextRequest();
+    }
+}
+
+
+
+
+// This function deletes the last chosen step, in case the user wants to undo a selected step.
+void StepManager::doBackStep()
+{
+    if (!selectedOptions.isEmpty()) {
+        selectedOptions.removeLast();
+        StepManager::stepCount--;
+        qDebug() << "Last step was removed.";
+        qDebug() << "TOTAL OF CURRENT STEPS: " + QString::number(stepCount);
+    } else {
+        qDebug() << "Could not delete last step, step list is empty.";
+    }
+
+    qDebug() << "Selected options after removal:";
+    for (const QString &currentOption : selectedOptions) {
+        qDebug() << "[" + currentOption + "] , ";
+    }
+}
+
+// This is the getter for the current step number.
+int StepManager::getStepToParse()
+{
+    return stepCount;
+}
---
+++ data/stepmanager.h
@@ -0,0 +1,26 @@
+#ifndef STEPMANAGER_H
+#define STEPMANAGER_H
+
+#include <QObject>
+#include <QString>
+
+class StepManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit StepManager(QObject *parent = nullptr);
+    int stepCount = 0;
+
+signals:
+    void stackNextRequest();
+
+public slots:
+    void setStep(QString currentStep);
+
+    void doNextStep();
+    void doBackStep();
+
+    int getStepToParse();   // returns MOD_STEP_X
+};
+
+#endif // STEPMANAGER_H
