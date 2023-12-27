a3d42293f71516d9075fce171f40ec4ce369580f
seifunaru <seifu.business@gmail.com>
2023-12-27 11:39:09 +0100

    Updated main cpp for connections foundation.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include <QQmlContext>
 #include <QQmlApplicationEngine>
 
+#include "data/stepManager.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    // Allow QML file read to get data from JSON files
     qputenv("QML_XHR_ALLOW_FILE_READ", "1");
+
+    // stepManager context to track on which step we are, and which options have been chosen.
+    StepManager *stepManager = new StepManager;
 
     QQmlApplicationEngine engine;
 
@@ -15,6 +20,8 @@ int main(int argc, char *argv[])
         &app, []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(QUrl::fromLocalFile("SIA/Main.qml"));
+
+    engine.rootContext()->setContextProperty("stepManager", stepManager);
 
     return app.exec();
 }
